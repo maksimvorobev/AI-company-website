cdea27c1059269f8157959bdfd96144b9d4240ef
maksimvorobev <maksdev505@gmail.com>
2025-06-12 13:24:21 +0700

    init: start website

---
+++ scripts/utils/defineScrollBarWidthCSSVar.js
@@ -0,0 +1,22 @@
+const defineScrollBarWidthCSSVar = () => {
+  const outerElement = document.createElement("div");
+  outerElement.style.visibility = "hidden";
+  outerElement.style.overflow = "scroll";
+
+  document.body.append(outerElement);
+
+  const innerElement = document.createElement("div");
+
+  outerElement.append(innerElement);
+
+  document.documentElement.style.setProperty(
+    "--scrollbar-width",
+    `${outerElement.offsetWidth - innerElement.offsetWidth}px`
+  );
+
+  console.log(document.documentElement)
+
+  outerElement.remove();
+};
+
+export default defineScrollBarWidthCSSVar;
